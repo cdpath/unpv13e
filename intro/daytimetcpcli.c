eda9fac5ff012a51aaebcd4bcc034f3a6a03639a
cdpath <cdpath@outlook.com>
2021-10-21 14:19:07 +0000

    add comments

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -10,18 +10,24 @@ main(int argc, char **argv)
 	if (argc != 2)
 		err_quit("usage: a.out <IPaddress>");
 
+	// 作为一种编码风格，作者总是在这样的两个左括号间加一个空格，提示比较运算的左侧同时也是一个赋值运算
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		err_sys("socket error");
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
+	// htons（“主机到网络短整数”）转换二进制端口号
 	servaddr.sin_port   = htons(13);	/* daytime server */
+	// inet_pton（“呈现形式到数值”）把ASCII命令行参数（例如运行本例子所用的206.168.112.96）转换为合适的格式
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
 
+	// connect函数应用于一个TCP套接字时，将与由它的第二个参数指向的套接字地址结构指定的服务器建立一个TCP连接
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
 
+	// 从TCP套接字读取数据时，我们总是需要把read编写在某个循环中，当read返回0（表明对端关闭连接）或负值（表明发生错误）时终止循环
+	// TCP本身并不提供记录结束标志：如果应用程序需要确定记录的边界，它就要自己去实现
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
 		recvline[n] = 0;	/* null terminate */
 		if (fputs(recvline, stdout) == EOF)
--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -13,20 +13,29 @@ main(int argc, char **argv)
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
+	// INADDR_ANY 表示服务器进程可以在任意一个网络接口上（当然前提是服务器有多个网络接口）接受客户端连接
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(13);	/* daytime server */
 
+	// bind 将服务器的 well-known port（这里是 13）绑定到上面创建的套接字上
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
+	// 将套接字转换为监听套接字，如此来自客户的连接可以在该套接字上由内核接受
+	// LISTENQ 来自 unp.h，定义了在监听描述符（listenfd）上排队的最大客户连接数
 	Listen(listenfd, LISTENQ);
 
+	// 无限循环
 	for ( ; ; ) {
+		// 服务器进程调用 accept 后即被睡眠，等待客户端连接的到达并被内核接受
+		// 细节是，TCP 三路握手之后，accept 返回已连接描述符（connfd），服务器通过 connfd 与客户端通信
 		connfd = Accept(listenfd, (SA *) NULL, NULL);
 
         ticks = time(NULL);
+		// snprintf函数在这个字符串末尾添加一个回车符和一个回行符
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
         Write(connfd, buff, strlen(buff));
 
+		// 服务端主动断开和客户端的连接
 		Close(connfd);
 	}
 }
